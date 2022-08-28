5f654a901cccfcdd73ea6c3180bec09080b26bef
3495936 <83470603+3495936@users.noreply.github.com>
2022-08-29 00:16:55 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,65 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-// Consultas:
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const getUserById = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
-
-const createUser = (request, response) => {
-  const { name, email } = request.body
-
-  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`User added with ID: ${results.insertId}`)
-  })
-}
-
-const updateUser = (request, response) => {
-  const id = (request.params.id)
-  const { name, email } = request.body
-
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User modified with ID: ${id}`)
-    }
-  )
-}
-
-const deleteUser = (request, response) => {
-  const id = (request.params.id)
-
-  pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`User deleted with ID: ${id}`)
-  })
-}
-
 // URLs GET, POST
 
 app.get("/", async (req, res) => {
@@ -97,8 +38,12 @@ app.get("/hora/", async (req, res) => {
   res.send(`La hora del servidor es: ${(hora.getHours()+2) + ":" + hora.getMinutes()+ ":" + hora.getSeconds()}`);
 });
 
-app.get('/users', getUsers)
-app.get('/users/:id', getUserById)
-app.post('/users', createUser)
-app.put('/users/:id', updateUser)
-app.delete('/users/:id', deleteUser)
+// Consultas:
+app.get('/users', async (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
