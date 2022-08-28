cdc96e98b159d6dadb6b6cebc6f9356f1547b8f3
3495936 <83470603+3495936@users.noreply.github.com>
2022-08-28 18:53:05 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,6 @@ app.listen(port, () => {
 
 app.get("/coche/", async (req, res) => {
   const { rows } = await pool.query("SELECT * from prueba");
-  var texto = rows.toString();
+  var texto = rows[0].nombre;
   res.send(`Resultado: ${texto}`);
 });
