53e099b1bacfa4a2b22dbc7c4f40367bf9aa53e4
3495936 <83470603+3495936@users.noreply.github.com>
2022-09-01 14:42:38 +0200

    posgresql

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,29 @@ app.use(bodyParser.text({ type: "text/html" }));
 // URLs GET, POST
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`PRIMOOOOOOOO. La hora de la BD: ${rows[0].now} `+req.query.id);
+    if (req.query.id !== null) {
+        if (req.query.fecha !== null) {
+        
+        } else {
+            const { profes } = await pool.query("SELECT id, nombre FROM usuarios LIMIT 500");
+            const { grupos } = await pool.query("SELECT grupo FROM grupos LIMIT 500");
+            const { horas } = await pool.query("SELECT hora FROM horas ORDER BY orden LIMIT 500");
+            const { guardiasUser } = await pool.query("SELECT diaHora FROM horarioGuardias WHERE profesor='"+req.query.id+"' LIMIT 500");
+            for (let i = 0; i < guardiasUser.length; i++) {
+                const { compas } = await pool.query("SELECT profesor, horas, puntos FROM horarioGuardias WHERE diaHora='"+guardiasUser[i].diaHora+"' LIMIT 500");
+                }
+            res.status(200).json('"p":'+profes);
+        }
+    }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get("/coche/", async (req, res) => {
-  const { rows } = await pool.query("SELECT * from prueba");
-  var texto = "";
-  for (let i = 0; i < rows.length; i++) {
-    texto += rows[i].nombre + " ";    
-  }
-  res.send(`Resultado: ${texto}`);
+app.get("/fecha/", async (req, res) => {
+  const { rows } = await pool.query("SELECT NOW()");
+  res.send(`PRIMOOOOOOOO. La hora de la BD: ${rows[0].now} `+req.query.id);
 });
 
 app.get("/hora/", async (req, res) => {
