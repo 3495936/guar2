b653884c4cd2925676f4ae98ad60b7f999151170
3495936 <83470603+3495936@users.noreply.github.com>
2022-09-03 17:13:16 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.get("/", async (req, res) => {
         } else {
             const { rows } = await pool.query("SELECT id, nombre FROM usuarios LIMIT 500");
             var profes = rows;
-            res.send(`p: ${json(rows)}`);
+            res.send(`p: ${JSON(rows)}`);
         }
     }
 });
