705258a47b2b3d5fbfedd7bdcf226f9cd7d87a64
3495936 <83470603+3495936@users.noreply.github.com>
2022-09-03 17:35:39 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,8 @@ app.get("/", async (req, res) => {
         if (req.query.fecha !== undefined) {
         
         } else {
-            let { rows } = await pool.query("SELECT id, nombre FROM usuarios LIMIT 500");
-            var profes = JSON.stringify(rows);            
-            rows  = await pool.query("SELECT grupo FROM grupos LIMIT 500");
+            const { rows } = await pool.query("SELECT id, nombre FROM usuarios LIMIT 500");
+            var profes = JSON.stringify(rows); 
             var grupos = JSON.stringify(rows);
             res.send(`p: ${profes}, g: ${grupos}`);
         }
